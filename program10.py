0500f8c548f6d1cb4826329b7a3caf64ca191f59
mp-mohit <pmohit47@gmail.com>
2017-02-14 22:08:37 +0530

    PROGRAMS 10 TO 11 DONE

---
+++ program10.py
@@ -0,0 +1,8 @@
+def simple_interest(principal,rate,time):
+    SI=(principal*rate*time)/100
+    print "Simple Interest is %d"%SI
+principal=input("Enter Principal Amount")
+rate=input("Enter Rate Of Interest")
+time=input("Enter Time")
+simple_interest(principal,rate,time)
+
